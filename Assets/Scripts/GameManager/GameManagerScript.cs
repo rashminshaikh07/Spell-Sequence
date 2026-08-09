50dfd400c75e264c8fb59719d26c8f985dc68704
rashminshaikh07 <rashminshaikh53@gmail.com>
2026-08-10 01:36:23 +0530

    fixed ui, timer, GameManager system

--- Assets/Scripts/GameManager/GameManagerScript.cs
+++ Assets/Scripts/GameManager/GameManagerScript.cs
@@ -2,15 +2,88 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManagerScript instance;
+
+    public int score = 0;
+    public int lives = 3;
+    public int round = 1;
+
+    public float roundTime = 10f;
+    private float timer;
+
+    public bool gameRunning = false;
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        StartGame();
+    }
+
     void Update()
     {
-        
+        if (!gameRunning) return;
+
+        timer -= Time.deltaTime;
+        UIManagerscript.instance.UpdateTimer(timer);
+
+        if (timer <= 0)
+        {
+            LoseLife();
+        }
+    }
+
+    public void StartGame()
+    {
+        score = 0;
+        lives = 3;
+        round = 1;
+
+        StartRound();
+        UIManagerscript.instance.UpdateUI(score, lives, round);
+    }
+
+    public void StartRound()
+    {
+        timer = roundTime;
+        gameRunning = true;
+
+        UIManagerscript.instance.UpdateRound(round);
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UIManagerscript.instance.UpdateScore(score);
+    }
+
+    public void NextRound()
+    {
+        round++;
+        StartRound();
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+        UIManagerscript.instance.UpdateLives(lives);
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            StartRound();
+        }
+    }
+
+    void GameOver()
+    {
+        gameRunning = false;
+        UIManagerscript.instance.ShowGameOver();
     }
 }
--- Assets/Scripts/GameManager/UIManagerscript.cs
+++ Assets/Scripts/GameManager/UIManagerscript.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using TMPro;
 
 public class UIManagerscript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static UIManagerscript instance;
+
+    public TMP_Text scoreText;
+    public TMP_Text livesText;
+    public TMP_Text roundText;
+    public TMP_Text timerText;
+
+    public GameObject gameOverPanel;
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateUI(int score, int lives, int round)
     {
-        
+        UpdateScore(score);
+        UpdateLives(lives);
+        UpdateRound(round);
+    }
+
+    public void UpdateScore(int score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateLives(int lives)
+    {
+        livesText.text = "Lives: " + lives;
+    }
+
+    public void UpdateRound(int round)
+    {
+        roundText.text = "Round: " + round;
+    }
+
+    public void UpdateTimer(float time)
+    {
+        if (timerText != null)
+            timerText.text = "Time: " + Mathf.Ceil(time).ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
